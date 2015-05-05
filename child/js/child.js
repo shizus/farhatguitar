9ee3b067bc6e175e26c5a83f2bb962b179156f60
Gabriel La Torre <latorregab@gmail.com>
2015-05-05 09:57:13 -0300

    Modifico estilo del home

--- child/js/child.js
+++ child/js/child.js
@@ -1,3 +1,6 @@
 jQuery(document).ready(function($) {
-  console.log("hola gato");
+  //var container= $("div.et_pb_slide_content");
+  //container.after().appendTo(container);
+  //$("a.et_pb_more_button").appendTo("div.et_pb_slide_content");
+//var content = $('a.et_pb_more_button').before(); content.append(content.after());
 })
